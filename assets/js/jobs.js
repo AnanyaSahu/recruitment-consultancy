db36a98e7753a4680ec6aa626fc51040e3727ceb
baran61 <azak.baran99@gmail.com>
2023-03-16 16:26:18 +0000

    New Changes

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -1,3 +1,5 @@
+var jobFeeds = []
+var jobIndex = 0
 
 function getjobVacancies(){  
     fetch('../assets/json/job-vacancy-json', {
@@ -9,22 +11,60 @@ function getjobVacancies(){
     .then(response => response.json())
     .then((data) =>
     {
-        // var json_data = {"2013-01-21":1,"2013-01-22":7};
-        var result = [];
+        for(var i in data) 
+        jobFeeds.push( data [i]);
 
-        for(var i in data)
-        result.push([i, data [i]]);
-
-        // console.log(result)
-        // for(var i in result)
-        // {
-        //     console.log(result[i][1])
-        // }
+        createContentOfJobFeed(jobFeeds)
+        // filterJobFeeds('jobType','Part-time')
     } ) 
 
-   }  
 
-   function getjobByJobId(jobId){  
+
+
+   }  
+   function navigateToDetailPage(ind) {
+    window.location.href = "./html/job-details.html";
+    jobIndex = 1
+
+}
+   function filterJobFeeds(filterType, filterValue) {
+    var filteredList = []
+
+    if(filterType == "jobCategory")
+    filteredList = jobFeeds.filter((jobfeed) => jobfeed.jobCategory.indexOf(filterValue) >-1)
+    
+    if(filterType == "jobType")
+    filteredList = jobFeeds.filter((jobfeed) => jobfeed.jobType.indexOf(filterValue) > -1)
+
+    console.log(filteredList)
+    createContentOfJobFeed(filteredList)
+
+   }
+
+
+   function createContentOfJobFeed(list) {
+    var jobPosting = document.getElementById("jobs-postings"); 
+    var ele = ""
+    jobPosting.innerHTML = ""
+    for(jobdata in list) {
+        job = list[jobdata]
+        ele += "<div class='card'><div class='card-content display-content'><h4 class=''>"+job.jobPosition +"</h4>"+
+        "<div class='display-flex'><h6> "+ job.companyName+"</h6> &nbsp;&nbsp;<h6>"+job.rating+"</h6> <i class='fa-solid fa-star'></i></div>" +
+        "<div class='display-flex'><i class='fa-solid fa-calendar'></i>&nbsp;<h6></h6>"+ job.experience+"</div>"+
+        "<div class='display-flex'><i class='fa-sharp fa-solid fa-location-dot'></i>  <span>"+job.location+"</span> </div>"+
+        "<div class='display-flex'><i class='fa-solid fa-calendar'></i> <span>Employment type:</span><span class='job-type'>"+
+        job.jobType+"</span>  &nbsp;<span id='job-category'>"+ job.jobCategory+"</span></div>"+     
+        "<hr>"+
+        "<div class='display-flex'>  <span>Posted </span><span id='posted-date'>"+job.postingDate+"</span><span></span> days ago</span></div>"
+        +"</div></div>"
+    }
+    jobPosting.innerHTML = ele
+   }
+
+
+   function getjobByJobId(){  
+    var jobId = jobIndex
+    console.log(jobId)
     fetch('../assets/json/job-vacancy-json', {
         method: 'GET',
         headers: {
@@ -39,19 +79,22 @@ function getjobVacancies(){
         for(var i in data)
         result.push([i, data [i]]);
 
-        console.log(result)
+        // console.log(result)
         for(var i in result)
         {
 
             if(result[i][1].jobId == jobId) {
                 var jobFound = result[i][1]
 
-                console.log(result[i][1])
+                // console.log(result[i][1])
             }
 
 
         }
     } ) 
+
+
+
 
    }  
 
@@ -76,12 +119,4 @@ function showToast() {
         x.style.display = "none"; 
     }, 1500);
 }
-
-function butto() {  
-
-    alert("You are being redirected to new page !");  
-
-} 
-
-
 
