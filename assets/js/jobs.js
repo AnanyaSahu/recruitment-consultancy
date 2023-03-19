8f2796b20d9310754ee8dd671447d8ae62ea759b
ananya <ananya.sahu0894@gmail.com>
2023-03-19 09:07:18 +0000

    REfactoring code and fixing CSS

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -1,152 +1,185 @@
 let jobFound = null
 let jobFeeds = []
+let jobFeedsCopy = []
 let jobIndex = 0
-function getjobVacancies(){  
-    console.log('getjobVacancies')
+
+let selectedFilter = { 'jobCategory': "null", 'jobType': "null" }
+function getjobVacancies() {
+    // console.log('getjobVacancies')
     fetch('../assets/json/job-vacancy-json', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
         },
     })
-    .then(response => response.json())
-    .then((data) =>
-    {
-        for(var i in data) 
-        jobFeeds.push( data [i]);
+        .then(response => response.json())
+        .then((data) => {
+            for (var i in data)
+                jobFeeds.push(data[i]);
 
-        createContentOfJobFeed(jobFeeds)
-        // filterJobFeeds('jobType','Part-time')
-    } ) 
-
-
+            jobFeedsCopy = JSON.parse(JSON.stringify(jobFeeds))
+            createContentOfJobFeed(jobFeeds)
+            // filterJobFeeds('jobType','Part-time')
+        })
 
 
-   }  
-   function navigateToDetailPage(ind) {
-    window.location.href = "./html/job-details.html";
-    jobIndex = 1
+
 
 }
-   function filterJobFeeds(filterType, filterValue) {
-    var filteredList = []
+function navigateToDetailPage(id) {
+    localStorage.setItem('id', id);
+    window.location.href = "./html/job-details.html";
+}
+function filterJobFeeds() {
+    var filteredList = JSON.parse(JSON.stringify(jobFeedsCopy))
+    console.log(selectedFilter)
 
-    if(filterType == "jobCategory")
-    filteredList = jobFeeds.filter((jobfeed) => jobfeed.jobCategory.indexOf(filterValue) >-1)
-    
-    if(filterType == "jobType")
-    filteredList = jobFeeds.filter((jobfeed) => jobfeed.jobType.indexOf(filterValue) > -1)
 
-    console.log(filteredList)
+    if (selectedFilter.jobCategory != "null") {
+        console.log(filteredList)
+        console.log(selectedFilter.jobCategory)
+        filteredList = filteredList.filter((jobfeed) => jobfeed.jobCategory.indexOf(selectedFilter.jobCategory) > -1)
+        console.log(filteredList)
+    }
+
+
+    if (selectedFilter.jobType != "null") {
+        console.log(filteredList)
+        console.log(selectedFilter.jobType)
+        filteredList = filteredList.filter((jobfeed) => jobfeed.jobType.indexOf(selectedFilter.jobType) > -1)
+        console.log(filteredList)
+    }
+
+
+
     createContentOfJobFeed(filteredList)
 
-   }
+}
 
 
-   function createContentOfJobFeed(list) {
-    var jobPosting = document.getElementById("jobs-postings"); 
+function selectFilterType(filterType, filterValue) {
+    if (filterType == "jobCategory") {
+        selectedFilter.jobCategory = filterValue
+    } else {
+        selectedFilter.jobType = filterValue
+    }
+}
+
+function clearFilterType() {
+    selectedFilter = {}
+    jobFeeds = jobFeedsCopy
+}
+
+function createContentOfJobFeed(list) {
+    var jobPosting = document.getElementById("jobs-postings");
     var ele = ""
     jobPosting.innerHTML = ""
-    for(jobdata in list) {
+    if(list.length == 0){
+        ele =" <div class='job-feed-header'><h3> <b> No Job matching your filter selection found </b> </h3></div>"
+    } 
+    else {
+    for (jobdata in list) {
         job = list[jobdata]
-        ele += "<div class='card'><div class='card-content display-content'><h4 class=''>"+job.jobPosition +"</h4>"+
-        "<div class='display-flex'><h6> "+ job.companyName+"</h6> &nbsp;&nbsp;<h6>"+job.rating+"</h6> <i class='fa-solid fa-star'></i></div>" +
-        "<div class='display-flex'><i class='fa-solid fa-calendar'></i>&nbsp;<h6></h6>"+ job.experience+"</div>"+
-        "<div class='display-flex'><i class='fa-sharp fa-solid fa-location-dot'></i>  <span>"+job.location+"</span> </div>"+
-        "<div class='display-flex'><i class='fa-solid fa-calendar'></i> <span>Employment type:</span><span class='job-type'>"+
-        job.jobType+"</span>  &nbsp;<span id='job-category'>"+ job.jobCategory+"</span></div>"+     
-        "<hr>"+
-        "<div class='display-flex'>  <span>Posted </span><span id='posted-date'>"+job.postingDate+"</span><span></span> days ago</span></div>"
-        +"</div></div>"
+        ele += "<div class='card'><div class='card-content display-content'><h4 class='' onclick='navigateToDetailPage(" + job.jobId + ")'>" + job.jobPosition + "</h4>" +
+            "<div class='display-flex'><h6> " + job.companyName + "</h6> &nbsp;&nbsp;<h6>" + job.rating + "</h6> <i class='fa-solid fa-star'></i></div>" +
+            "<div class='display-flex'><i class='fa-solid fa-calendar'></i>&nbsp;<h6></h6>" + job.experience + "</div>" +
+            "<div class='display-flex'><i class='fa-sharp fa-solid fa-location-dot'></i>  <span>" + job.location + "</span> </div>" +
+            "<div class='display-flex'><i class='fa-solid fa-calendar'></i> <span>Employment type:</span><span class='job-type'>" +
+            job.jobType + "</span>  &nbsp;<span id='job-category'>" + job.jobCategory + "</span></div>" +
+            "<hr>" +
+            "<div class='display-flex'>  <span>Posted &nbsp;</span><span id='posted-date'>" + calculateDays(job.postingDate) + " &nbsp;</span><span></span> days ago</span></div>"
+            + "</div></div>"
+            
     }
+    ele += "<div class='last-card'></div>"
+
+    }
+
+    
+
     jobPosting.innerHTML = ele
-   }
+}
 
 
-   function getjobByJobId(jobId){  
-    console.log('jobFound')
-    // var jobId = jobIndex
-    // console.log(jobFeeds)
+function getjobByJobId(jobId) {
     fetch('../assets/json/job-vacancy-json', {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
         },
     })
-    .then(response => response.json())
-    .then((data) =>
-    {
-        var result = [];
+        .then(response => response.json())
+        .then((data) => {
+            var result = [];
+            for (var i in data)
+                result.push(data[i]);
 
-        for(var i in data)
-        result.push([i, data [i]]);
-
-        // console.log(result)
-        for(var i in result)
-        {
-
-            if(result[i][1].jobId == jobId) {
-
-                jobFound = result[i][1]
-
-                
-                console.log(jobFound)
-                showJobDetails(jobFound);
+            for (var i in result) {
+                if (result[i].jobId == jobId) {
+                    jobFound = result[i]
+                    showJobDetails(jobFound);
+                }
             }
-
-
-        }
-    } ) 
-
-
-
-
-   }  
-
-
-function applyJob() {
-    var modal = document.getElementById("apply-modal-container");
-    modal.style.display = "block";
+        })
 }
 
-  
+
+// function applyJob() {
+//     var modal = document.getElementById("apply-modal-container");
+//     modal.style.display = "block";
+// }
+
+
 
 function closeModal() {
+
     var modal = document.getElementById("apply-modal-container");
     modal.style.display = "none";
 }
 
-function showToast() {
-  var error = document.getElementById("error-msg");
-  error.innerHTML = "";
-  phonePattern = /^[0-9]{10}$/;
-  emailPattern =  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // Validation paatern taken from https://regexr.com/3e48o
-  let fullName = document.forms["jobApplicationFrom"]["name"].value;
-  let phone = document.forms["jobApplicationFrom"]["phone"].value;
-  let email = document.forms["jobApplicationFrom"]["email"].value;
-  if (fullName.trim() == "" || phone.trim() == "" || email.trim() == "" ) {
-    error.innerHTML = "Please fill all the required details";
-  }
-  else if (phone.trim() != "" && !phonePattern.test(phone.trim()) ) {
-    console.log(phonePattern.test(phone.trim()))
-    error.innerHTML = "Please enter correct phone";
-  }
+$(document).ready(function () {
+    // $("#close-btn").click(function(){
+    //   $("#apply-modal-container").hide();
+    // });
 
-   else if (email.trim() != "" && !emailPattern.test(email.trim()) ) {
-    console.log(emailPattern.test(email.trim()))
-    console.log(email.trim() != "")
-    error.innerHTML = "Please enter correct email";
-  }
-  else {
-    error.innerHTML = ""
-    var x = document.getElementById("toastNotification");
-    document.getElementById("jobApplicationFrom").reset();
-    closeModal()
-    x.style.display = "block"; 
-    setTimeout(() => {
-        x.style.display = "none"; 
-    }, 1500);
-  }
+    $("#apply-modal-btn").click(function () {
+        $("#apply-modal-container").show();
+    });
+
+
+});
+
+function showToast() {
+    var error = document.getElementById("error-msg");
+    error.innerHTML = "";
+    phonePattern = /^[0-9]{10}$/;
+    emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // Validation paatern taken from https://regexr.com/3e48o
+    let fullName = document.forms["jobApplicationFrom"]["name"].value;
+    let phone = document.forms["jobApplicationFrom"]["phone"].value;
+    let email = document.forms["jobApplicationFrom"]["email"].value;
+    if (fullName.trim() == "" || phone.trim() == "" || email.trim() == "") {
+        error.innerHTML = "Please fill all the required details";
+    }
+    else if (phone.trim() != "" && !phonePattern.test(phone.trim())) {
+        console.log(phonePattern.test(phone.trim()))
+        error.innerHTML = "Please enter correct phone";
+    }
+
+    else if (email.trim() != "" && !emailPattern.test(email.trim())) {
+        console.log(emailPattern.test(email.trim()))
+        console.log(email.trim() != "")
+        error.innerHTML = "Please enter correct email";
+    }
+    else {
+        error.innerHTML = ""
+        var x = document.getElementById("toastNotification");
+        document.getElementById("jobApplicationFrom").reset();
+        closeModal()
+        x.style.display = "block";
+        setTimeout(() => {
+            x.style.display = "none"; 
+        }, 1500);
+    }
 
 
 
@@ -154,9 +187,6 @@ function showToast() {
 }
 
 function showJobDetails() {
-    
-    console.log("showJobDetails")
-    console.log(jobFound.jobPosition)
     var jobposition = document.getElementById("job-position");
     jobposition.innerHTML = jobFound.jobPosition
 
@@ -187,7 +217,7 @@ function showJobDetails() {
     var hiringinsight = document.getElementById("hiring-insight");
     hiringinsight.innerHTML = jobFound.hiring
 
-    
+
     var skills = document.getElementById("job-skills");
     skills.innerHTML = jobFound.skills.join(', ');
 
@@ -198,49 +228,53 @@ function showJobDetails() {
     rating.innerHTML = jobFound.rating;
 
     var postdate = document.getElementById("post-date");
-    const today = new Date();
-    const posteddate = new Date(jobFound.postingDate);
-    var timeDifference = today.getTime() - posteddate.getTime();
-    var days =  timeDifference / (1000 * 3600 * 24);
-    postdate.innerHTML = Math.abs(Math.round(days))
-    generatePErksandBenefits() 
-    
-  
+    postdate.innerHTML = calculateDays(jobFound.postingDate);
+
+    generatePErksandBenefits()
+
+
 }
 
 function generatePErksandBenefits() {
-    
-   var eleToAdd = ""
-    for (perk  in  jobFound.jobPerksAndBenifits) {
-        if(jobFound.jobPerksAndBenifits[perk] == "Team Outings") {
+    var eleToAdd = ""
+    for (perk in jobFound.jobPerksAndBenifits) {
+        if (jobFound.jobPerksAndBenifits[perk] == "Team Outings") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/TeamOuting.jpg' alt='benefit-img'><div class='benefit-label'>Team Outings</div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/TeamOuting.jpg' alt='benefit-img'><div class='benefit-label'>Team Outings</div></div></div>"
         }
-        if(jobFound.jobPerksAndBenifits[perk] == "Work From Home") {
+        if (jobFound.jobPerksAndBenifits[perk] == "Work From Home") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/WFH.jpg' alt='benefit-img'><div class='benefit-label'>Work From Home</div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/WFH.jpg' alt='benefit-img'><div class='benefit-label'>Work From Home</div></div></div>"
         }
-        if(jobFound.jobPerksAndBenifits[perk] == "Soft Skill Training") {
+        if (jobFound.jobPerksAndBenifits[perk] == "Soft Skill Training") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/SoftSkillsTraining.jpg' alt='benefit-img'><div class='benefit-label'>Soft Skill Training</div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/SoftSkillsTraining.jpg' alt='benefit-img'><div class='benefit-label'>Soft Skill Training</div></div></div>"
         }
-        if(jobFound.jobPerksAndBenifits[perk] == "International Relocation") {
+        if (jobFound.jobPerksAndBenifits[perk] == "International Relocation") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Education.jpg' alt='benefit-img'><div class='benefit-label'>International Relocation</div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Education.jpg' alt='benefit-img'><div class='benefit-label'>International Relocation</div></div></div>"
         }
-        if(jobFound.jobPerksAndBenifits[perk] == "Health Insurance") {
+        if (jobFound.jobPerksAndBenifits[perk] == "Health Insurance") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Transportation.jpg' alt='benefit-img'><div class='benefit-label'>Health Insurance</div></div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Transportation.jpg' alt='benefit-img'><div class='benefit-label'>Health Insurance</div></div></div></div>"
         }
-        if(jobFound.jobPerksAndBenifits[perk] == "Cafateria") {
+        if (jobFound.jobPerksAndBenifits[perk] == "Cafateria") {
             console.log(perk)
-            eleToAdd +=   "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Transportation.jpg' alt='benefit-img'><div class='benefit-label'>Cafateria</div></div></div></div>"
+            eleToAdd += "<div class='benefit'><div class='benefit-inside'> <img class='benefit-img' src='https://static.ambitionbox.com/img/emailers/Transportation.jpg' alt='benefit-img'><div class='benefit-label'>Cafateria</div></div></div></div>"
         }
-      }
+    }
 
     var per = document.getElementById("perks");
     per.innerHTML = eleToAdd
 
-    
-  
+
+
+}
+
+function calculateDays(postingDate) {
+    const today = new Date();
+    const posteddate = new Date(postingDate);
+    var timeDifference = today.getTime() - posteddate.getTime();
+    var days = timeDifference / (1000 * 3600 * 24);
+    return Math.abs(Math.round(days))
 }
