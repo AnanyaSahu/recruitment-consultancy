b267a9086d800a5da0d8024e530c1757e060eaa8
ananya <ananya.sahu0894@gmail.com>
2023-03-19 10:53:14 +0000

    Adding comments and refactoring code

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -2,10 +2,17 @@ let jobFound = null
 let jobFeeds = []
 let jobFeedsCopy = []
 let jobIndex = 0
-
 let selectedFilter = { 'jobCategory': "null", 'jobType': "null" }
+
+// show job application modal
+$(document).ready(function () {
+    $("#apply-modal-btn").click(function () {
+        $("#apply-modal-container").show();
+    });
+});
+
+// get job list from JSON
 function getjobVacancies() {
-    // console.log('getjobVacancies')
     fetch('../assets/json/job-vacancy-json', {
         method: 'GET',
         headers: {
@@ -16,47 +23,34 @@ function getjobVacancies() {
         .then((data) => {
             for (var i in data)
                 jobFeeds.push(data[i]);
-
+            // create deep copy of job feed list
             jobFeedsCopy = JSON.parse(JSON.stringify(jobFeeds))
             createContentOfJobFeed(jobFeeds)
-            // filterJobFeeds('jobType','Part-time')
         })
-
-
-
-
 }
+
+// method to navigate to Job Details page
 function navigateToDetailPage(id) {
     localStorage.setItem('id', id);
     window.location.href = "./html/job-details.html";
 }
-function filterJobFeeds() {
-    var filteredList = JSON.parse(JSON.stringify(jobFeedsCopy))
-    console.log(selectedFilter)
 
+// filter job feed based on criteria
+function filterJobFeeds() {
+    // create deep copy of job feed copy list and reassign to filtered list in case if the filter parameter is empty
+    var filteredList = JSON.parse(JSON.stringify(jobFeedsCopy))
 
     if (selectedFilter.jobCategory != "null") {
-        console.log(filteredList)
-        console.log(selectedFilter.jobCategory)
         filteredList = filteredList.filter((jobfeed) => jobfeed.jobCategory.indexOf(selectedFilter.jobCategory) > -1)
-        console.log(filteredList)
     }
-
-
     if (selectedFilter.jobType != "null") {
-        console.log(filteredList)
-        console.log(selectedFilter.jobType)
         filteredList = filteredList.filter((jobfeed) => jobfeed.jobType.indexOf(selectedFilter.jobType) > -1)
-        console.log(filteredList)
     }
-
-
-
     createContentOfJobFeed(filteredList)
 
 }
 
-
+// set selected filter to selectedFilter disctionary
 function selectFilterType(filterType, filterValue) {
     if (filterType == "jobCategory") {
         selectedFilter.jobCategory = filterValue
@@ -65,42 +59,35 @@ function selectFilterType(filterType, filterValue) {
     }
 }
 
-function clearFilterType() {
-    selectedFilter = {}
-    jobFeeds = jobFeedsCopy
-}
-
+// create dynamic job feed
 function createContentOfJobFeed(list) {
     var jobPosting = document.getElementById("jobs-postings");
     var ele = ""
     jobPosting.innerHTML = ""
-    if(list.length == 0){
-        ele =" <div class='job-feed-header'><h3> <b> No Job matching your filter selection found </b> </h3></div>"
-    } 
+    // if job list is empty show no content msg to user
+    if (list.length == 0) {
+        ele = " <div class='job-feed-header'><h3> <b> No Job matching your filter selection found </b> </h3></div>"
+    }
     else {
-    for (jobdata in list) {
-        job = list[jobdata]
-        ele += "<div class='card'><div class='card-content display-content'><h4 class='' onclick='navigateToDetailPage(" + job.jobId + ")'>" + job.jobPosition + "</h4>" +
-            "<div class='display-flex'><h6> " + job.companyName + "</h6> &nbsp;&nbsp;<h6>" + job.rating + "</h6> <i class='fa-solid fa-star'></i></div>" +
-            "<div class='display-flex'><i class='fa-solid fa-calendar'></i>&nbsp;<h6></h6>" + job.experience + "</div>" +
-            "<div class='display-flex'><i class='fa-sharp fa-solid fa-location-dot'></i>  <span>" + job.location + "</span> </div>" +
-            "<div class='display-flex'><i class='fa-solid fa-calendar'></i> <span>Employment type:</span><span class='job-type'>" +
-            job.jobType + "</span>  &nbsp;<span id='job-category'>" + job.jobCategory + "</span></div>" +
-            "<hr>" +
-            "<div class='display-flex'>  <span>Posted &nbsp;</span><span id='posted-date'>" + calculateDays(job.postingDate) + " &nbsp;</span><span></span> days ago</span></div>"
-            + "</div></div>"
-            
+        for (jobdata in list) {
+            job = list[jobdata]
+            ele += "<div class='card'><div class='card-content display-content'><h4 class='cursor-pointer' onclick='navigateToDetailPage(" + job.jobId + ")'>" + job.jobPosition + "</h4>" +
+                "<div class='display-flex'><h6> " + job.companyName + "</h6> &nbsp;&nbsp;<h6>" + job.rating + "</h6> <i class='fa-solid fa-star'></i></div>" +
+                "<div class='display-flex'><i class='fa-solid fa-calendar'></i>&nbsp;<h6></h6>" + job.experience + "</div>" +
+                "<div class='display-flex'><i class='fa-sharp fa-solid fa-location-dot'></i>  <span>" + job.location + "</span> </div>" +
+                "<div class='display-flex'><i class='fa-solid fa-calendar'></i> <span>Employment type:</span><span class='job-type'>" +
+                job.jobType + "</span>  &nbsp;<span id='job-category'>" + job.jobCategory + "</span></div>" +
+                "<hr>" +
+                "<div class='display-flex'>  <span>Posted &nbsp;</span><span id='posted-date'>" + calculateDays(job.postingDate) + " &nbsp;</span><span></span> days ago</span></div>"
+                + "</div></div>"
+
+        }
+        ele += "<div class='last-card'></div>"
     }
-    ele += "<div class='last-card'></div>"
-
-    }
-
-    
-
     jobPosting.innerHTML = ele
 }
 
-
+// find job by job id for job detail page
 function getjobByJobId(jobId) {
     fetch('../assets/json/job-vacancy-json', {
         method: 'GET',
@@ -123,69 +110,49 @@ function getjobByJobId(jobId) {
         })
 }
 
-
-// function applyJob() {
-//     var modal = document.getElementById("apply-modal-container");
-//     modal.style.display = "block";
-// }
-
-
-
+// close modal method to hide job application modal
 function closeModal() {
-
     var modal = document.getElementById("apply-modal-container");
     modal.style.display = "none";
 }
 
-$(document).ready(function () {
-    // $("#close-btn").click(function(){
-    //   $("#apply-modal-container").hide();
-    // });
-
-    $("#apply-modal-btn").click(function () {
-        $("#apply-modal-container").show();
-    });
-
-
-});
-
+// validate job application form values and show toast notification on successfull application
 function showToast() {
     var error = document.getElementById("error-msg");
     error.innerHTML = "";
     phonePattern = /^[0-9]{10}$/;
     emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // Validation paatern taken from https://regexr.com/3e48o
+
     let fullName = document.forms["jobApplicationFrom"]["name"].value;
     let phone = document.forms["jobApplicationFrom"]["phone"].value;
     let email = document.forms["jobApplicationFrom"]["email"].value;
-    if (fullName.trim() == "" || phone.trim() == "" || email.trim() == "") {
-        error.innerHTML = "Please fill all the required details";
-    }
-    else if (phone.trim() != "" && !phonePattern.test(phone.trim())) {
-        console.log(phonePattern.test(phone.trim()))
-        error.innerHTML = "Please enter correct phone";
-    }
 
-    else if (email.trim() != "" && !emailPattern.test(email.trim())) {
+    if (fullName.trim() == "" || phone.trim() == "" || email.trim() == "") {
+        error.innerHTML = "<span>Please fill all the required details</span>";
+    } else if (phone.trim() != "" && !phonePattern.test(phone.trim())) {
+        console.log(phonePattern.test(phone.trim()))
+        error.innerHTML = "<span>Please enter correct phone</span>";
+    } else if (email.trim() != "" && !emailPattern.test(email.trim())) {
         console.log(emailPattern.test(email.trim()))
         console.log(email.trim() != "")
-        error.innerHTML = "Please enter correct email";
+        error.innerHTML = "<span>Please enter correct email</span>";
     }
     else {
+        // all form values pass validation
         error.innerHTML = ""
         var x = document.getElementById("toastNotification");
         document.getElementById("jobApplicationFrom").reset();
         closeModal()
+        // show toast notification for successful application
         x.style.display = "block";
+        // hide toast notification for successful application after 1500ms
         setTimeout(() => {
-            x.style.display = "none"; 
+            x.style.display = "none";
         }, 1500);
     }
-
-
-
-
 }
 
+// create content for job details page
 function showJobDetails() {
     var jobposition = document.getElementById("job-position");
     jobposition.innerHTML = jobFound.jobPosition
@@ -217,7 +184,6 @@ function showJobDetails() {
     var hiringinsight = document.getElementById("hiring-insight");
     hiringinsight.innerHTML = jobFound.hiring
 
-
     var skills = document.getElementById("job-skills");
     skills.innerHTML = jobFound.skills.join(', ');
 
@@ -230,12 +196,12 @@ function showJobDetails() {
     var postdate = document.getElementById("post-date");
     postdate.innerHTML = calculateDays(jobFound.postingDate);
 
-    generatePErksandBenefits()
-
-
+    generatePerksandBenefits()
 }
 
-function generatePErksandBenefits() {
+// create dynamic content perks and benifits cards 
+// Created using refernce from https://www.naukri.com/job-listings-android-developer-2-4-years-jayam-solutions-hyderabad-secunderabad-0-to-1-years-180323003954?src=homepage_personalized
+function generatePerksandBenefits() {
     var eleToAdd = ""
     for (perk in jobFound.jobPerksAndBenifits) {
         if (jobFound.jobPerksAndBenifits[perk] == "Team Outings") {
@@ -266,11 +232,9 @@ function generatePErksandBenefits() {
 
     var per = document.getElementById("perks");
     per.innerHTML = eleToAdd
-
-
-
 }
 
+// calculate how many days ago the job was posted from today
 function calculateDays(postingDate) {
     const today = new Date();
     const posteddate = new Date(postingDate);
